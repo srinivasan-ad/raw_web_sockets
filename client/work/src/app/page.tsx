dcd469c2155ab0e97f112bdfcbdfcaf69bb1dece
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-01-28 00:36:06 +0530

    Raw ws

--- client/work/src/app/page.tsx
+++ client/work/src/app/page.tsx
@@ -1,5 +1,36 @@
-import Image from "next/image";
+'use client'
+import React, { useEffect, useState } from 'react'
+
 
 export default function Home() {
-  return 
+  const [socket,setSocket] = useState(0);
+    function sendMessage()
+    {
+       if(!socket)
+       {
+        return;
+       }
+       else{
+  
+        socket.send("ping");
+       }
+    }
+    useEffect(() => {
+     fetch("ws://localhost:8080")
+     const ws = new WebSocket("ws://localhost:8080")
+     setSocket(ws);
+     ws.onmessage = (ev) => {
+      alert(ev.data)
+
+     }
+
+    },[])
+  return (
+<div className="flex h-screen w-full items-center bg-red-200 justify-center">
+<div className=' flex  flex-col items-center justify-center h-[400px] w-[400px] gap-y-10'>
+<input type='text' placeholder='Messages...' className='py-6 px-16  w-80 h-20  rounded-md'></input>
+<button onClick={sendMessage} className='rounded-md w-20 h-12 bg-green-500' >Send</button>
+</div>
+</div>
+  )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,15 @@ const ws = new WebSocketServer({ port: 8080 });
 //event handler
 ws.on("connection", function (socket) {
     console.log("user connected !")
-  setInterval(() => {
-    socket.send("stock rate is " + Math.random());
-  }, 2000);
+//   setInterval(() => {
+//     socket.send("stock rate is " + Math.random());
+//   }, 2000);
   //ping pong application
   socket.on("message",(e) => {
     if(e.toString() == "ping")
     {
         socket.send("pong");
     }
-    // console.log(e.toString());
+    console.log(e.toString());
   })
 });
