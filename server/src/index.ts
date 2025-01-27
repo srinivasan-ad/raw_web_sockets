078ea5134da103bdf69d3a07c4969ad2dd0b3cc1
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-01-27 21:43:44 +0530

    Raw ws

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,11 @@ import { WebSocketServer } from "ws";
 const ws = new WebSocketServer({ port: 8080 });
 //event handler
 ws.on("connection", function (socket) {
+    console.log("user connected !")
   setInterval(() => {
     socket.send("stock rate is " + Math.random());
   }, 2000);
+  socket.on("message",(e) => {
+    console.log(e);
+  })
 });
