f62509ad04aa9a6d08ee57de1442ab3ff2f62b22
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-01-27 21:38:06 +0530

    Raw ws

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,8 @@
 import { WebSocketServer } from "ws";
-const ws = new WebSocketServer({port : 8080});
+const ws = new WebSocketServer({ port: 8080 });
 //event handler
-ws.on("connection",function(socket)
-{
-    setInterval(() => {
-
-        socket.send("hello");
-    },2000)
-})
+ws.on("connection", function (socket) {
+  setInterval(() => {
+    socket.send("hello");
+  }, 2000);
+});
