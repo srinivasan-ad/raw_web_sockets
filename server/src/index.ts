fa24c9f96386ab1f410bc998950d381c2406344f
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-01-27 21:44:03 +0530

    Raw ws

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,6 @@ ws.on("connection", function (socket) {
     socket.send("stock rate is " + Math.random());
   }, 2000);
   socket.on("message",(e) => {
-    console.log(e);
+    console.log(e.toString());
   })
 });
